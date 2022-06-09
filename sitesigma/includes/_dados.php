653f6c2b37535194b8939aad0d9368673af984b9
aueiikkkj <93769917+aueiikkkj@users.noreply.github.com>
2022-06-09 12:27:03 -0300

    for

---
+++ sitesigma/includes/_dados.php
@@ -0,0 +1,18 @@
+<?php
+
+$produtos = array (
+    0 => array('nome' => 'Capivara', 'preco' => 'Pra você é de graça', 'descricao' => 'Fofa, igual você', 'imagem' => 'capivara.jpg'),
+    1 => array('nome' => 'Pietro', 'preco' => 'Troll face', 'descricao' => '44kg no supino, péssimo no vôlei', 'imagem' => 'pietro.jfif'),
+    2 => array('nome' => '', 'preco' => '', 'descricao' => '', 'imagem' => ''),
+    3 => array('nome' => '', 'preco' => '', 'descricao' => '', 'imagem' => ''),
+    4 => array('nome' => '', 'preco' => '', 'descricao' => '', 'imagem' => ''),
+    5 => array('nome' => '', 'preco' => '', 'descricao' => '', 'imagem' => ''),
+    6 => array('nome' => '', 'preco' => '', 'descricao' => '', 'imagem' => ''),
+    7 => array('nome' => '', 'preco' => '', 'descricao' => '', 'imagem' => ''),
+    8 => array('nome' => '', 'preco' => '', 'descricao' => '', 'imagem' => ''),
+    9 => array('nome' => '', 'preco' => '', 'descricao' => '', 'imagem' => '')
+
+);
+
+
+?>
